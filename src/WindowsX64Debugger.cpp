f055c0d8ac790b7eeecc61c61a289667e713852d
siinzn <msinannoufal@gmail.com>
2026-04-28 17:42:20 +0400

    Fixed condition for exiting loop, other minor fixes

--- src/WindowsX64Debugger.cpp
+++ src/WindowsX64Debugger.cpp
@@ -1,7 +1,7 @@
 ﻿#include "WindowsX64Debugger.h"
 
-#define DBG_CONTINUE ((DWORD)0x00010002L)
 #define DEBUG_PROCESS 0x00000001
+#define DEBUG_ONLY_THIS_PROCESS 0x00000002
 
 using namespace std;
 
@@ -16,12 +16,11 @@ int main()
 	ZeroMemory(&si, sizeof(si));
 	si.cb = sizeof(si);
 	ZeroMemory(&pi, sizeof(pi));
-	if (!CreateProcessA(applicationName, NULL, NULL, NULL, FALSE, DEBUG_PROCESS, NULL, NULL, &si, &pi));
+	if (!CreateProcessA(applicationName, NULL, NULL, NULL, FALSE, DEBUG_ONLY_THIS_PROCESS, NULL, NULL, &si, &pi)) return 1;
 	while (running) {
-		//std::cout << "Success! - " << pi.dwThreadId << " - " << pi.dwProcessId << "\n";
 		WaitForDebugEvent(&de, INFINITE);
 		std::cout << de.dwDebugEventCode << "\n";
-		if (!EXIT_PROCESS_DEBUG_EVENT) running = FALSE;
+		if (de.dwDebugEventCode == EXIT_PROCESS_DEBUG_EVENT) running = FALSE;
 		ContinueDebugEvent(de.dwProcessId, de.dwThreadId, DBG_CONTINUE);
 	};
 	CloseHandle(pi.hProcess);
